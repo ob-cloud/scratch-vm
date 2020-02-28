0db035c64373c9b7ae284307083e49637e9e8632
eamiear <justcall_me_k@163.com>
2020-02-28 11:16:25 +0800

    uav block vm functions

--- src/blocks/scratch3_uav.js
+++ src/blocks/scratch3_uav.js
@@ -22,7 +22,17 @@ class Scratch3UavBlocks {
         return {
             uav_calibration: this.calibration,
             uav_text: this.text,
-            uav_operator: this.changeOperator
+            uav_operator: this.changeOperator,
+            uav_cal: this.cal,
+            uav_lock: this.lock,
+            uav_unlock: this.unlock,
+            uav_takeoff: this.takeoff,
+            uav_landing: this.landing,
+            uav_fly_rise: this.flyRise,
+            uav_fly_down: this.flyDown,
+            uav_fly_direction: this.flyDirection,
+            uav_fly_turn: this.flyturn,
+            uav_send_message: this.sendMsg
         };
     }
     calibration (args, util) {
@@ -46,6 +56,58 @@ class Scratch3UavBlocks {
         }
         console.log(operator, num1, num2)
         return operators[operator]
+    }
+    cal (args, util) {
+        const cal = {
+            'gyro': 0x01,
+            'mag': 0x02
+        }
+        const operator = Cast.toString(args.UAVCAL).toLowerCase();
+        // cmd
+        return cal[operator] || ''
+    }
+    lock () {
+        return 0xee
+    }
+    unlock () {
+        return 0x03
+    }
+    takeoff () {
+        return 0x04
+    }
+    landing () {
+        return 0x04
+    }
+    flyRise (args) {
+        // data1
+        return [0xed, Cast.toNumber(args.NUM)]
+    }
+    flyDown () {
+        // data2
+        return [0xed, Cast.toNumber(args.NUM)]
+    }
+    flyDirection (args) {
+        const operator = Cast.toString(args.UAVFLYDIRECTION).toLowerCase();
+        const direction = {
+            'forward': [0xed, Cast.toNumber(args.NUM)], //data3
+            'back': [0xed, Cast.toNumber(args.NUM)], //data4
+            'left': [0xed, Cast.toNumber(args.NUM)], //data5
+            'right': [0xed, Cast.toNumber(args.NUM)], //data6
+        }
+        return direction[operator]
+    }
+    flyturn (args) {
+        const operator = Cast.toString(args.UAVFLYTURN).toLowerCase();
+        const turn = {
+            'left': [0xed, Cast.toNumber(args.NUM)], //data7
+            'right': [0xed, Cast.toNumber(args.NUM)], //data8
+        }
+        return turn[operator]
+    }
+    sendMsg (args) {
+        const message = Cast.toString(args.MSG).toLowerCase();
+        // dt
+        return message
     }
 }
 
